3f84f061668edb70c9c7de355bf87e5370ce631e
Dareczek <dareczek06@gmail.com>
2024-03-07 21:04:34 +0100

    Create animation for the navigation menu

--- news-homepage/main.js
+++ news-homepage/main.js
@@ -1,1 +1,37 @@
 import './scss/style.scss'
+
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 69.375em');
+const navContent = document.querySelector('.nav__content');
+const navOverlay = document.querySelector('.nav__overlay');
+
+
+function openMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'true');
+}
+function closeMobileMenu() {
+    btnClose.setAttribute('aria-expanded', 'false');
+}
+
+
+function setupNav(e) {
+    if (e.maches) {     
+        setTimeout(() => {
+            navContent.style.display = 'block';
+            navOverlay.style.display = 'block';
+        }, 500);
+    } else {
+        navContent.style.display = '';
+    }
+}
+
+setupNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+media.addEventListener('change', function (e) {
+    setupNav(e);
+}
+);
